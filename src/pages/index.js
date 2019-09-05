d712ed9a9261f4c986255220f1adc48a0660a48c
Wojtek Zając <wojtek@wojtekzajac.com>
2019-09-05 15:25:10 +0300

    Add a basic class component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,39 @@
 import React from "react"
-import Header from "../components/header"
 
-export default () => (
-  <>
-    <Header
-      workshopNumber={4234}
-      newProperty={"Testing"}
-    />
-    <div
-      style={{ color: "red" }}
-    >
-      Hello SwahiliBox!
-    </div>
-  </>
-)
+class Swahilibox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0,
+    }
+  }
+
+  render() {
+    const resetCounter = () => this.setState({ index: 0 })
+
+    return (
+      <div>
+        <h3>I am a class component!</h3>
+        <p>Current index: {this.state.index}</p>
+        <button
+          onClick={() =>
+            this.setState({
+              index: this.state.index + 1,
+            })
+          }
+        >
+          Bump index
+        </button>
+        <button onClick={resetCounter}>Reset</button>
+      </div>
+    )
+  }
+}
+
+class SecondClass extends React.Component {
+  render() {
+    return <div>Second class</div>
+  }
+}
+
+export default Swahilibox
